ddfd6c30edf30224005bc0a782b54e5f835c15b6
artiveloper <artiveloper@gmail.com>
2026-04-15 21:31:25 +0900

    refactor: og-image 를 추가하고 이미지 경로를 public 으로 이동한다.

--- apps/web/components/daycare/common/Header.tsx
+++ apps/web/components/daycare/common/Header.tsx
@@ -2,13 +2,12 @@
 
 import Image from 'next/image';
 import Link from 'next/link';
-import logo from '@/app/logo.png';
 
 export function Header() {
     return (
         <header className="fixed top-0 left-0 right-0 z-50 h-14 bg-white border-b border-gray-200 flex items-center px-4">
             <Link href="/">
-                <Image src={logo} alt="키즐리" height={28} priority />
+                <Image src="/logo.png" alt="키즐리" width={60} height={28} priority />
             </Link>
         </header>
     );
