818cab7ccd2e568a710edfdf4d53455d9c66f173
artiveloper <artiveloper@gmail.com>
2026-07-19 00:10:38 +0900

    design: 로고 크기 변경

--- apps/web/app/terms/page.tsx
+++ apps/web/app/terms/page.tsx
@@ -37,7 +37,7 @@ export default function TermsPage() {
         <div className="min-h-screen bg-white">
             <header className="fixed top-0 left-0 right-0 z-50 h-14 bg-white border-b border-gray-200 flex items-center px-4">
                 <Link href="/">
-                    <Image src="/logo.png" alt="키즐리" width={60} height={25} priority />
+                    <Image src="/logo.png" alt="키즐리" width={80} height={34} priority />
                 </Link>
             </header>
 
